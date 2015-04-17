eb8199798382f7ee80bd31da0470b1fd05271f01
Igor Matkovic <igor@cod3.me>
2015-04-17 14:03:59 +0100

    add

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -5,13 +5,13 @@ class ExampleTest extends PHPUnit_Framework_TestCase {
     public function testExamplee() {
         $exmp = new Example();
         fwrite(STDERR,  $exmp->la());
-        $this->assertEquals('burel', 'neee', $exmp->la());
+        $this->assertEquals('burel',  $exmp->ola(), $exmp->la());
 
     }
     public function testExample() {
         $exmp = new Example();
         fwrite(STDERR,  $exmp->ola());
-        $this->assertEquals('burel', 'neee', $exmp->ola());
+        $this->assertEquals('burel',  $exmp->ola(), $exmp->ola());
 
     }
 }
