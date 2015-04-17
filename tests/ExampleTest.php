0e8ab37f9b9468bd08b8582baa6515a76b4c830d
Igor Matkovic <igor@cod3.me>
2015-04-17 13:55:09 +0100

    tr

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -4,12 +4,15 @@ require_once('Example.php');
 class ExampleTest extends PHPUnit_Framework_TestCase {
     public function testExamplee() {
         $exmp = new Example();
+        fwrite(STDERR,  $exmp->la());
         $this->assertEquals('burel', 'neee', $exmp->la());
 
     }
     public function testExample() {
         $exmp = new Example();
+        fwrite(STDERR,  $exmp->ola());
         $this->assertEquals('burel', 'neee', $exmp->ola());
+
     }
 }
 ?>
