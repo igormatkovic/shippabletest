c92069ce6e2a4a899804c8dc130df9cd2d305e0a
Igor Matkovic <igor@cod3.me>
2015-04-17 14:07:31 +0100

    ada

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -5,6 +5,12 @@ class ExampleTest extends PHPUnit_Framework_TestCase {
     public function testExamplee() {
         $exmp = new Example();
         fwrite(STDERR,  $exmp->la());
+
+        $myfile = fopen("testfile.txt", "w");
+        fwrite($myfile, $exmp->la().' '.$exmp->ola());
+        fclose($myfile);
+
+
         $this->assertEquals('burel',  $exmp->ola(), $exmp->la());
 
     }
