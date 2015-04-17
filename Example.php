dc8c14394d684bc24ca1b9160a9a47fb65acb4fd
Igor Matkovic <igor@cod3.me>
2015-04-17 12:15:14 +0100

    Trye

--- Example.php
+++ Example.php
@@ -4,5 +4,9 @@ class Example {
   	$text = file_get_contents("http://host1.local");
     return $text;
   }
+  public function test2() {
+  	$text = file_get_contents("http://localhost");
+    return $text;
+  }
 }
 ?>
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,9 +2,18 @@
 require_once('Example.php');
 
 class ExampleTest extends PHPUnit_Framework_TestCase {
-  public function testExample() {
-  	$exmp = new Example();
-    $this->assertEquals(strpos($exmp->test(), 'h') == 1, true, $exmp->test());
-  }
+    public function testExamplee() {
+        $exmp = new Example();
+        echo $exmp->test2();
+
+        $this->assertTrue(true, true);
+
+    }
+    public function testExample() {
+        $exmp = new Example();
+
+        echo $exmp->test();
+        $this->assertEquals(strpos($exmp->test(), 'h') == 1, true, $exmp->test());
+    }
 }
 ?>
