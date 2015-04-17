7499f5ab22e98ab6568ff489f04be23e0d268c89
Igor Matkovic <igor@cod3.me>
2015-04-17 12:06:07 +0100

    what

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,9 +1,10 @@
 <?php
 require_once('Example.php');
+
 class ExampleTest extends PHPUnit_Framework_TestCase {
   public function testExample() {
   	$exmp = new Example();
-    $this->assertEquals(strpos($exmp->test(), 'h') == 1, true);
+    $this->assertEquals(strpos($exmp->test(), 'h') == 1, true, $exmp->test());
   }
 }
 ?>
