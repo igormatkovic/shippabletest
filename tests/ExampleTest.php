2b3c4f5fe3d658945236e03c333e8197bc930324
Igor Matkovic <igor@cod3.me>
2015-04-24 09:51:34 +0100

    tr

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -4,19 +4,22 @@ require_once('Example.php');
 class ExampleTest extends PHPUnit_Framework_TestCase {
     public function testExamplee() {
         $exmp = new Example();
-        fwrite(STDERR,  $exmp->la());
 
-        $myfile = fopen("testfile.txt", "w");
-        fwrite($myfile, $exmp->la().' '.$exmp->ola());
-        fclose($myfile);
-
+        $file = 'testing.txt';
+        $current = file_get_contents($file);
+        $current .= $exmp->la()."\n";
+        file_put_contents($file, $current);
 
         $this->assertEquals('burel',  $exmp->ola(), $exmp->la());
 
     }
     public function testExample() {
         $exmp = new Example();
-        fwrite(STDERR,  $exmp->ola());
+
+        $file = 'testing.txt';
+        $current = file_get_contents($file);
+        $current .= $exmp->ola()."\n";
+        file_put_contents($file, $current);
         $this->assertEquals('burel',  $exmp->ola(), $exmp->ola());
     }
 }
